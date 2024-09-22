ff5686a49bfc6637a88938bf86372c6afc257757
Venkata Alapati <VAlapati@lineagelogistics.com>
2024-09-22 12:42:25 -0700

    Lab 2 Release

--- lab2_fa24/shazam_utils.py
+++ lab2_fa24/shazam_utils.py
@@ -3,8 +3,27 @@ import csv
 
 from scipy.io import wavfile
 
+
+HASHES_PER_PEAK = 15
+
+
 def generate_hash(f1, f2, t1, t2, hash_length = 20):
     td = t2 - t1
     h = hashlib.sha1(("%s|%s|%s" % (str(f1), str(f2), str(td))).encode("utf-8"))
     return h.hexdigest()[0:hash_length], t1
 
+
+def hashing(t1, f1, time_idx, freq_idx):
+    times, freqs = t1[time_idx], f1[freq_idx]
+    sorted_peaks = sorted(zip(freqs, times), key=lambda x: x[1])
+    hashes = []
+
+    for i in range(len(sorted_peaks)):
+        for j in range(1, HASHES_PER_PEAK + 1):
+            if i + j >= len(sorted_peaks):
+                break
+            f_1, t_1 = sorted_peaks[i]
+            f_2, t_2 = sorted_peaks[i + j]
+            hashes.append(generate_hash(f_1, f_2, t_1, t_2))
+
+    return hashes
